75c774fd97113dba2c2808e24ac53cea91b0fe33
xunchangqing <xunchangqing@qq.com>
2015-06-29 09:36:01 +0800

    remove players

--- app/models/match.rb
+++ app/models/match.rb
@@ -21,8 +21,8 @@ class Match < ActiveRecord::Base
   validates :name, presence: true
   validates :time, presence: true
   validates :event_id, presence: true
-  validates :player1_id, presence: true
-  validates :player2_id, presence: true
+  #validates :player1_id, presence: true
+  #validates :player2_id, presence: true
 
   #def winner_str
     #@winner_str
